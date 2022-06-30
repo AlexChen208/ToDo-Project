75bbf70b023a948f6720eb746392f73d28da1f6e
AlexChen208 <alex.chencs207@gmail.com>
2022-06-30 14:06:11 -0700

    delete functionality for specific item

--- controllers/lists.js
+++ controllers/lists.js
@@ -14,9 +14,17 @@ function create(req, res) {
     })
 }
 
-function deleteToDo(req, res) {
-    // Find list item inside category 
-    List.findByIdAndDelete(req.params.id, function(err, lists) {
+async function deleteToDo(req, res, next) {
+    try {
+        const category = await Category.findById(req.params.id)
+        console.log('CATEGORY', category)
+        if (!category) return res.redirect('/categories')
+        let list = category.lists.id(req.params.listId)
+        console.log('LIST', list)
+        category.lists.remove(list)
+        await category.save()
         res.redirect(`/categories/${category._id}`)
-    })
+    } catch(err) {
+        return next(err)
+    }
 }
--- routes/categories.js
+++ routes/categories.js
@@ -9,13 +9,13 @@ router.get('/new', categoriesCtrl.new)
 // route to index page
 router.get('/',  isLoggedIn, categoriesCtrl.index)
 // route to specific category
-router.get('/:id', categoriesCtrl.show)
+router.get('/:id', isLoggedIn, categoriesCtrl.show)
 // route to add data to home page
 router.post('/', isLoggedIn, categoriesCtrl.create)
 // route to delete
 router.delete('/:id', categoriesCtrl.delete)
 // route to edit
-router.get('/:id/edit', categoriesCtrl.edit)
+router.get('/:id/edit', isLoggedIn, categoriesCtrl.edit)
 // update route
 router.put('/:id', categoriesCtrl.update)
 
