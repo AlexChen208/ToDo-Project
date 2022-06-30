45bbe7373c8c49167e8771eb0b8d823618126022
AlexChen208 <alex.chencs207@gmail.com>
2022-06-30 15:23:38 -0700

    styling

--- models/category.js
+++ models/category.js
@@ -1,30 +1,31 @@
 const mongoose = require('mongoose')
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
-
-
+ 
+ 
 const listSchema = new Schema({
-    listItem: {
-        type: String
-    }
+   listItem: {
+       type: String
+   }
 })
-
+ 
 const categoriesSchema = new Schema({
-    title: {
-        type: String
-    },
-    dueDate: {
-        type: Date,
-        default: function() {
-          let d = new Date();
-          let month = d.getMonth();
-          let day = d.getDate();
-          let result = new Date(month, day);
-          return result;
-        }
-    },
-    lists: [listSchema]
+   title: {
+       type: String
+   },
+   dueDate: {
+       type: Date,
+       default: function() {
+         let d = new Date();
+         let month = d.getMonth();
+         let day = d.getDate();
+         let result = new Date(month, day);
+         return result;
+       }
+   },
+   lists: [listSchema]
 })
-
-
+ 
+ 
 module.exports = mongoose.model('Category', categoriesSchema)
+
